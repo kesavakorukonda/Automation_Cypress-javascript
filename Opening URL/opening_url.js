6b25b162b31b5b3cd69f546f132d28fc62ec5cd4
kesava korukonda <58676518+kesavakorukonda@users.noreply.github.com>
2020-04-12 19:46:29 +0530

    Add files via upload

---
+++ Opening URL/opening_url.js
@@ -0,0 +1,11 @@
+describe('Opening URL Test suite', function()
+{
+    it('Opening URL Test case', function()
+    {
+        cy.visit("https://www.flipkart.com/");
+    }
+    )
+}
+)
+
+//Running a simple test to open a website.
